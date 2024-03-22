ecdc97a3d6ea71cb5730699a07103751ddb05a51
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-03-22 04:52:38 +0000

    グループ実装中　viewファイルnewのみ

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,13 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
+    @comment.book_id = book.id
     @comment.save
   end  
   
   def destroy
     @comment = BookComment.find(params[:id])
-    @book = @comment.book
     @comment.destroy
   end  
   
